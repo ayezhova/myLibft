da0720274033a55b25bafb28dd6f1cdf195f9276
ayezhova <52675053+ayezhova@users.noreply.github.com>
2019-10-03 17:02:44 -0700

    Add files via upload

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,13 +6,22 @@
 /*   By: ayezhova <ayezhova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 13:53:56 by ayezhova          #+#    #+#             */
-/*   Updated: 2019/10/03 16:57:53 by ayezhova         ###   ########.fr       */
+/*   Updated: 2019/10/03 17:01:42 by ayezhova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+/*
 #include "libft.h"
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+} */
+
+#include "libft.h"
+
+void	ft_putchar(char c)
+{
+	ft_putchar_fd(c, 1);
 }
